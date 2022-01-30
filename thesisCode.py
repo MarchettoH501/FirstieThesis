6d6aa2ff3fc55cf4b919e002ce2ecc351181e812
MarchettoH501 <marcocristiano.risi@gmail.com>
2022-01-29 21:55:31 -0500

    ugh

--- thesisCode.py
+++ thesisCode.py
@@ -44,15 +44,21 @@ def sign(a,b,c,d,e,f,g,h):
   else: 
     return (1)
 
-def mod2 (a):
-  if a ==2:
-    a= 0
-  else:
-    a==a
-
 def generator_group (g1,g2,h1,h2):
-  g1g2= [mod2(g1[0]+g2[0]),mod2(g1[1]+g2[1]),mod2(g1[2]+g2[2]),mod2(g1[3]+g2[3]), sign(g1[0],g1[1],g1[2],g1[3],g2[0],g2[1],g2[2],g2[3])]
-  h1h2= [mod2(h1[0]+h2[0]),mod2(h1[1]+h2[1]),mod2(h1[2]+h2[2]),mod2(h1[3]+h2[3]), sign(h1[0],h1[1],h1[2],h1[3],h2[0],h2[1],h2[2],h2[3])]
+  g1g2= [(g1[0]+g2[0]),(g1[1]+g2[1]),(g1[2]+g2[2]),(g1[3]+g2[3]), sign(g1[0],g1[1],g1[2],g1[3],g2[0],g2[1],g2[2],g2[3])]
+  h1h2= [(h1[0]+h2[0]),(h1[1]+h2[1]),(h1[2]+h2[2]),(h1[3]+h2[3]), sign(h1[0],h1[1],h1[2],h1[3],h2[0],h2[1],h2[2],h2[3])]
+  if g1g2[1]==2:
+    g1g2[1]=0
+  elif g1g2[2]==2:
+    g1g2[2]=0
+  elif g1g2[3]==2:
+    g1g2[3]=0
+  elif h1h2[1]==2:
+    h1h2[1]=0
+  elif h1h2[2]==2:
+    h1h2[2]=0
+  elif h1h2[3]==2:
+    h1h2[3]=0
   return(g1g2,h1h2)
   #print(g1g2)
   #change to mod addition
@@ -64,19 +70,18 @@ def trace(i,j,k,l,g1g2,h1h2):
     for j in range(0,2):
       for k in range(0,2):
         for l in range(0,2):
-  
-          P= [i*g1g2[0],i*g1g2[1],j*g1g2[2],j*g1g2[3],k*h1h2[0],k*h1h2[1],l*h1h2[2],l*h1h2[3]]
-          if P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3]==h1h2[3]:
-            trace.append(4)
-          elif P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3] != h1h2[3]:
-            trace.append(-4)
-          else: 
-            trace.append(0)
-          #return(trace)
-          #print(trace)
-          trace_sum= sum(trace)
-          #print(trace_sum)   
-          return(trace_sum)
+            P= [i*g1g2[0],i*g1g2[1],j*g1g2[2],j*g1g2[3],k*h1h2[0],k*h1h2[1],l*h1h2[2],l*h1h2[3]]
+            if P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3]==h1h2[3]:
+              trace.append(4)
+            elif P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3] != h1h2[3]:
+              trace.append(-4)
+            else: 
+              trace.append(0)
+            #return(trace)
+            #print(trace)
+            trace_sum= sum(trace)
+            #print(trace_sum)   
+            return(trace_sum)
 
 #print(len(A))
 g1=[]
@@ -94,6 +99,7 @@ def search (a,b,c,d):
           h1==(A[t])
           h2==(A[v])
           theList.append(trace(a,b,c,d,generator_group(g1,g2,h1,h2)))
-  return(max(theList))
+    return(max(theList))
+print(search(1,1,1,1))
 
 
