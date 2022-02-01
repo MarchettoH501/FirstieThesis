6d5753e23d0d7e0503e2a5ea28a55a590696e7a4
MarchettoH501 <marcocristiano.risi@gmail.com>
2022-02-01 11:54:35 -0500

    after meeting

--- 1Feb22.py
+++ 1Feb22.py
@@ -36,9 +36,10 @@ def sign(a,b,c,d,e,f,g,h):
   else: 
     return (1)
 
-def generator_group (g1,g2,h1,h2):
-  g1g2= [(g1[0]+g2[0]),(g1[1]+g2[1]),(g1[2]+g2[2]),(g1[3]+g2[3]), sign(g1[0],g1[1],g1[2],g1[3],g2[0],g2[1],g2[2],g2[3])]
-  h1h2= [(h1[0]+h2[0]),(h1[1]+h2[1]),(h1[2]+h2[2]),(h1[3]+h2[3]), sign(h1[0],h1[1],h1[2],h1[3],h2[0],h2[1],h2[2],h2[3])]
+def generator_group (g1,g2,h1,h2): 
+  g1g2= [(g1[0]+g2[0])%2,(g1[1]+g2[1])%2,(g1[2]+g2[2])%2,(g1[3]+g2[3])%2, sign(g1[0],g1[1],g1[2],g1[3],g2[0],g2[1],g2[2],g2[3])]
+  h1h2= [(h1[0]+h2[0])%2,(h1[1]+h2[1])%2,(h1[2]+h2[2])%2,(h1[3]+h2[3])%2, sign(h1[0],h1[1],h1[2],h1[3],h2[0],h2[1],h2[2],h2[3])]
+  '''
   if g1g2[1]==2:
     g1g2[1]=0
   elif g1g2[2]==2:
@@ -51,46 +52,53 @@ def generator_group (g1,g2,h1,h2):
     h1h2[2]=0
   elif h1h2[3]==2:
     h1h2[3]=0
-  return(g1g2,h1h2)
+  '''
+ 
   #print(g1g2)
   #change to mod addition
   #just create based questions (sing based on answrs for the generator)
 
-def trace(i,j,k,l,g1g2,h1h2):
-  trace=[]
-  for i in range(0,2):
-    for j in range(0,2):
-      for k in range(0,2):
-        for l in range(0,2):
-          P= [i*g1g2[0],i*g1g2[1],j*g1g2[2],j*g1g2[3],k*h1h2[0],k*h1h2[1],l*h1h2[2],l*h1h2[3]]
-          if P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3]==h1h2[3]:
-            trace.append(4)
-          elif P[0] == P[4] and P[1] == P[5] and P[2] == P[6] and P[3] == P[7] and g1g2[3] != h1h2[3]:
-            trace.append(-4)
-          else: 
-            trace.append(0)
-  #return(trace)
-  #print(trace)
-  trace_sum= sum(trace)
-  #print(trace_sum)   
-  return(trace_sum)
+def trace(g1,g2,h1,h2):
+    trace=[]
+    G= [(g1[0]+g2[0])%2,(g1[1]+g2[1])%2,(g1[2]+g2[2])%2,(g1[3]+g2[3])%2,(g1[4]+g2[4])%2 ]
+    H= [(h1[0]+h2[0])%2,(h1[1]+h2[1])%2,(h1[2]+h2[2])%2,(h1[3]+h2[3])%2,(h1[4]+h2[4])%2]
+    for i in range(0,2):
+      for j in range(0,2):
+        for k in range(0,2):
+          for l in range(0,2):
+            '''
+            on't trat it as a2x5, it  a 1x5
+            '''
+            if i*G[0] == k*H[0] and i*G[1] == k*H[1] and j*G[2] == l*H[2] and j*G[3] == l*H[3] and G[4] == H[4]:
+              trace.append(4)
+            elif i*G[0] == k*H[0] and i*G[1] == k*H[1] and j*G[2] == l*H[2] and j*G[3] == l*H[3] and G[4] != H[4]:
+              trace.append(-4)
+            else: 
+              trace.append(0)
+    #return(G,H,trace)
+    #print(trace)
+    trace_sum= sum(trace)
+    #print(trace_sum)   
+    return(trace_sum)
 
 #print(len(A))
 g1=[]
 g2=[]
 h1=[]
 h2=[]
-def search (a,b,c,d):
+def search ():
   theList=[]
-  for r in range(0,51):
-    for s  in range(0,51):
+  for r in range(0,len(A)):
+    for s  in range(0,len(A)):
       g1=(A[r][0])
       g2=(A[r][1])
       h1=(A[s][0])
       h2=(A[s][1])
-      theList.append(trace(a,b,c,d,generator_group(g1,g2,h1,h2)[0],generator_group(g1,g2,h1,h2)[1]))
+      theList.append(trace(g1,g2,h1,h2))
     return(theList)
-print(search(1,1,1,1))
+theList=[]
+print(search())
+
 
 '''
 Found the error: its when I select g1 and g2, and h1 and h2
