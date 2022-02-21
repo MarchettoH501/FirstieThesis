d3ea9612de913d5a0faef2c877eadb1e18d99bfc
MarchettoH501 <marcocristiano.risi@gmail.com>
2022-02-21 12:18:37 -0500

    I think this works

--- CHSH.py
+++ CHSH.py
@@ -8,18 +8,16 @@ for i in range(0,2):
            for o in range(0,2):
              for p in range(0,2):
               if i+j+k+l>=1 and n+o+p>=1:
-                for m in range(0,2):
-                  for n in range(0,2):
                     if k==1 and i==0 and j==0 and  k!=n and k!=o:
                       if p==1 and p!=l:
-                        A.append([[i,j,k,l,m],[0,n,o,p,n]])
+                        A.append([[i,j,k,l],[0,n,o,p]])
                     elif l==1 and l!=n and l!=o and l!=p and l==j==k==0:
-                      A.append([[i,j,k,l,m],[0,n,o,p,n]])
+                      A.append([[i,j,k,l],[0,n,o,p]])
                     elif j==1 and n!=j:
                       if o==1 and k!=0: 
-                        A.append([[i,j,k,l,m],[0,n,o,p,n]])
+                        A.append([[i,j,k,l],[0,n,o,p]])
                       elif p==1 and o ==0 and l==0:
-                        A.append([[i,j,k,l,m],[0,n,o,p,n]])
+                        A.append([[i,j,k,l],[0,n,o,p]])
 def trace(g1,g2,h1,h2):
     trace=[]
     for i in range(0,2):
@@ -37,7 +35,13 @@ def trace(g1,g2,h1,h2):
               trace.append(-4)
             else: 
               trace.append(0)
-            return(trace)
+            #return(trace)
+
+    #return(G,H,trace)
+    #print(trace)
+    trace_sum= sum(trace)
+    #print(trace_sum)   
+    return(trace_sum)
 #print(len(A))
 g1=[]
 g2=[]
@@ -53,13 +57,12 @@ def search ():
       h2=(A[s][1])
       theList.append(trace(g1,g2,h1,h2))
     return(theList)
-theList=[]
-print(search())
+"theList=[]"
+"print(search())"
 
 q1= random.randint(0,1)
 q2= random.randint(0,1)
 def Vchsh(a,b,x,y):
-
   if x==1 and y ==1:
     if a==b: 
       return(0)
@@ -72,44 +75,37 @@ def Vchsh(a,b,x,y):
         return(0)
 
 def answers(g1,g2,h1,h2):
-  if g1==g2: 
-    a= 1
+  if g1[4]==g2[4]: 
+    a= 0
   else:
-    a=0
-  if h1==h2: 
-    b=1
-  else:
+    a=1
+  if h1[4]==h2[4]: 
     b=0
+  else:
+    b=1
   return(a,b)
-
-def play(x,y):
+  
+g1=[1, 0, 0, 0]
+g2=[0, 1, 0, 0]
+h1=[1,0,0,0]
+h2=[0,0,0,1]
+def play(g1,g2,h1,h2,x,y):
   w=[]
-  A=[] # extend list to include non leading 1s mod 
-  for i in range(0,2):
-    for j in range(0,2):
-      for k in range(0,2):
-        for l in range(0,2):
-          for n in range(0,2):
-            for o in range(0,2):
-              for p in range(0,2):
-                if i+j+k+l>=1 and n+o+p>=1:
-                  for m in range(0,2):
-                    for n in range(0,2):
-                      if k==1 and i==0 and j==0 and  k!=n and k!=o:
-                        if p==1 and p!=l:
-                          A.append([[i,j,k,l,m],[0,n,o,p,n]])
-                      elif l==1 and l!=n and l!=o and l!=p and l==j==k==0:
-                        A.append([[i,j,k,l,m],[0,n,o,p,n]])
-                      elif j==1 and n!=j:
-                        if o==1 and k!=0: 
-                          A.append([[i,j,k,l,m],[0,n,o,p,n]])
-                        elif p==1 and o ==0 and l==0:
-                          A.append([[i,j,k,l,m],[0,n,o,p,n]])
-  for r in range(0,len(A)):
-    for s  in range(0,len(A)):
-      g1=(A[r][0])
-      g2=(A[r][1])
-      h1=(A[s][0])
-      h2=(A[s][1])
-      trace(g1,g2,h1,h2)*(Vchsh(answers(g1,g2,h1,h2)[0],answers(g1,g2,h1,h2)[1],x,y))
-   
+  streak=[]
+  for a1 in range(0,2):
+    for a2 in range(0,2):
+      for b1 in range(0,2):
+        for b2 in range(0,2):
+          g1.append(a1)
+          g2.append(a2)
+          h1.append(b1)
+          h2.append(b2)
+          w.append(trace(g1,g2,h1,h2)*(Vchsh(answers(g1,g2,h1,h2)[0],answers(g1,g2,h1,h2)[1],x,y))/(16))
+          win= sum(w)/len(w)
+          streak.append(win)
+          g1.remove(g1[4])
+          g2.remove(g2[4])
+          h1.remove(h1[4])
+          h2.remove(h2[4])
+  print(streak) 
+play(g1,g2,h1,h2,1,1)
